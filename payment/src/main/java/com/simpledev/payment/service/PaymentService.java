5da1a32c13a301be51da2719b7d37a5d4c3b7b80
Filipe Soares <filipecarlossoares@gmail.com>
2022-06-04 11:33:09 -0300

    add docker compose

--- payment/src/main/java/com/simpledev/payment/service/PaymentService.java
+++ payment/src/main/java/com/simpledev/payment/service/PaymentService.java
@@ -1,7 +1,8 @@
 package com.simpledev.payment.service;
 
+import com.simpledev.payment.exception.PaymentNotFoundException;
 import com.simpledev.payment.model.Payment;
 
 public interface PaymentService {
-    public Payment payOrder(Long orderId);
+    public Payment payOrder(Long orderId) throws PaymentNotFoundException;
 }
--- payment/src/main/java/com/simpledev/payment/service/PaymentServiceImpl.java
+++ payment/src/main/java/com/simpledev/payment/service/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.simpledev.payment.service;
 
+import com.simpledev.payment.enums.PaymentStatus;
+import com.simpledev.payment.exception.PaymentNotFoundException;
 import com.simpledev.payment.model.Payment;
 import com.simpledev.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,9 +13,10 @@ public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
     @Override
-    public Payment payOrder(Long orderId) {
-        //Payment payment = paymentRepository.save();
-        //return payment;
-        return null;
+    public Payment payOrder(Long orderId) throws PaymentNotFoundException {
+        var optionalPayment = paymentRepository.getByOrderId(orderId);
+        var payment = optionalPayment.orElseThrow(PaymentNotFoundException::new);
+        payment.setStatus(PaymentStatus.PAID);
+        return payment;
     }
 }
